ec87593084e6a3fd5ad89bdde658465e0b2875f6
reklov_w <reklov_w@5852f345-3237-4a1a-914e-b28fb4781291>
2011-11-23 18:43:42 +0000

    branch release-2-0-8 deleted, new roll-out soon

--- branches/release-2-0-8/saga-gis/src/saga_core/saga_api/saga_api_to_python_linux.sh
+++
@@ -1,9 +0,0 @@
-
-echo "SWIG compilation..."
-swig -c++ -python -includeall -D_SAGA_PYTHON ./saga_api.h
-echo "SWIG compilation finished."
-
-echo "Python compilation..."
-python saga_api_to_python_linux.py install
-cp ./saga_api.py /usr/lib/python/site-packages/
-echo "Python compilation finished."
